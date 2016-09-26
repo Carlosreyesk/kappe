f6d5da39a0ee61e72480f75dc38a20ad1e198d27
carlosreyesk <carlosreyesk@gmail.com>
2016-09-26 15:45:11 -0600

    modal btn

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,15 @@ $(document).ready(function(){
 
     var filterBtn = document.querySelector('.js-open-filter');
     filterBtn.addEventListener('click', openFilter);
+    var filterBtnFinish = document.querySelector('.js-close-filter');
+    filterBtnFinish.addEventListener('click', closeFilter);
+    var modal = document.querySelector('.js-filter');
 
     function openFilter(){
-        var modal = document.querySelector('.js-filter');
         modal.classList.remove('hidden');
+    }
+
+    function closeFilter(){
+        modal.classList.add('hidden');
     }
 });
