6f873b9e5b3d4ec7bf5a4e65fca1e34c90782cea
carlosreyesk <carlosreyesk@gmail.com>
2016-10-04 12:03:38 -0600

    cors

--- api/index.js
+++ api/index.js
@@ -15,4 +15,11 @@ app.listen(8080, function(err){
 	console.log('server');
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', routes);
+
